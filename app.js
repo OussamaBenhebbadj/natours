26712126df1075dd7ccd87c9e39dfc2691f2db28
Oussama Ben Hebbadj <lo_benhebbadj@esi.dz>
2025-07-09 19:31:35 +0100

    patch , delete

--- app.js
+++ app.js
@@ -6,12 +6,32 @@ const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simpl
 app.use(express.json()); //midleware to parse JSON bodies
 
 //endpoint
-app.get("/api/v1/tours",(req,res)=>{
+app.get("/api/v1/tours",(req,res)=>{ //get all tours
     res.status(200).json({
         status: "success",
         results: tours.length,
         data: {
             tours
+        }
+    });
+})
+
+app.get("/api/v1/tours/:id",(req,res)=>{ //get one tour
+    //console.log(req.params); //to see the parameters in the URL
+    //req.params is an object that contains the parameters in the URL
+    //e.g. if the URL is /api/v1/tours/5, req.params will be { id: '5' 
+    const id = req.params.id * 1; //convert string to number   
+    if(id > tours.length) { //if the id is greater than the number of tours
+        return res.status(404).json({ //return 404 error
+            status: "fail",
+            message: "Invalid ID"
+        });
+    }
+    const tour = tours.find(el => el.id === id); //find the tour with the given id
+    res.status(200).json({
+        status: "success",
+        data: {
+            tour
         }
     });
 })
@@ -27,6 +47,34 @@ app.post("/api/v1/tours",(req,res)=>{ //create a new tour
                 tour: newTour
             }
         });
+    });
+});
+
+app.patch("/api/v1/tours/:id",(req,res)=>{ //update a tour
+    const id = req.params.id * 1; //convert string to number
+    if(id > tours.length) { 
+        return res.status(404).json({ 
+            status: "fail",
+            message: "Invalid ID"
+        });
+    }
+    res.status(200).json({
+        status: "success",
+        message: "Updated tour successfully"
+    });
+});
+
+app.delete("/api/v1/tours/:id",(req,res)=>{ //delete a tour
+    const id = req.params.id * 1; //convert string to number
+    if(id > tours.length) { 
+        return res.status(404).json({ 
+            status: "fail",
+            message: "Invalid ID"
+        });
+    }
+    res.status(204).json({
+        status: "success",
+        data: null
     });
 });
 
