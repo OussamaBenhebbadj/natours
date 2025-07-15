5c2018931b146677148d324415ab320987db1828
Oussama Ben Hebbadj <lo_benhebbadj@esi.dz>
2025-07-15 18:23:05 +0100

    schema , model

--- server.js
+++ server.js
@@ -18,3 +18,33 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
+
+const TourSchema = mongoose.Schema({
+  name:{
+    type: String,
+    required: [true,'A Tour must have a name'],
+    unique: true
+  },
+  price:{
+    type: Number,
+    required: [true,'A Tour must have a price'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  }
+});
+
+const Tour = mongoose.model('Tour', TourSchema); 
+
+const TourTest = new Tour({
+  name: "The Park Camber",
+  price : 797,
+  rating: 4.8
+});
+
+TourTest.save().then(doc => {
+  console.log(doc);
+}).catch(err => {
+  console.log('ERROR : ', err);
+})
