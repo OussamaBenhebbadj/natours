d67bb02cc2e31e7d91cd4dbcca876a2718e67a4d
Oussama Ben Hebbadj <lo_benhebbadj@esi.dz>
2025-07-19 14:33:56 +0100

    CRUD

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -3,76 +3,94 @@ const Tour = require("./../models/tourModels");
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'utf-8'));
 
-const getAllTours = (req,res)=> { //get all tours
-    res.status(200).json({
+const getAllTours = async (req,res)=> { //get all tours
+    try{
+        const tours = await Tour.find();
+        res.status(200).json({
         status: "success",
         results: tours.length,
         data: {
             tours
         }
     });
+    }catch(err){
+        res.status(400).json({
+        status: "fail",
+        message: err
+    });
+    }  
 };
 
-const getOneTour = (req,res)=>{ //get one tour
+const getOneTour = async (req,res)=>{ //get one tour
     //console.log(req.params); //to see the parameters in the URL
     //req.params is an object that contains the parameters in the URL
     //e.g. if the URL is /api/v1/tours/5, req.params will be { id: '5' 
-    const id = req.params.id * 1; //convert string to number   
-    if(id > tours.length) { //if the id is greater than the number of tours
-        return res.status(404).json({ //return 404 error
+    try{
+        const tour = await Tour.findById(req.params.id);
+        res.status(200).json({
+            status: "success",
+            data: {
+                tour
+            }
+        });
+    } catch(err) {
+        res.status(400).json({
             status: "fail",
-            message: "Invalid ID"
+            message: err
         });
     }
-    const tour = tours.find(el => el.id === id); //find the tour with the given id
-    res.status(200).json({
-        status: "success",
-        data: {
-            tour
-        }
-    });
 };
 
-const createTour = (req,res)=>{ //create a new tour
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({id: newId}, req.body);
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+const createTour = async (req,res)=>{ //create a new tour
+    try{
+        const newTour = await Tour.create(req.body);
         res.status(201).json({
             status: "success",
             data: {
                 tour: newTour
             }
         });
-    });
-};
-
-const updateTour = (req,res)=>{ //update a tour
-    const id = req.params.id * 1; //convert string to number
-    if(id > tours.length) { 
-        return res.status(404).json({ 
+    } catch(err){
+        res.status(400).json({
             status: "fail",
-            message: "Invalid ID"
+            message: err
         });
     }
-    res.status(200).json({
-        status: "success",
-        message: "Updated tour successfully"
-    });
 };
 
-const deleteTour = (req,res)=>{ //delete a tour
-    const id = req.params.id * 1; //convert string to number
-    if(id > tours.length) { 
-        return res.status(404).json({ 
-            status: "fail",
-            message: "Invalid ID"
+const updateTour = async (req,res)=>{ //update a tour
+   try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id , req.body ,{
+            new: true,
+            runValidators: true
         });
-    }
-    res.status(204).json({
-        status: "success",
-        data: null
-    });
+        res.status(200).json({
+            status: "success",
+            data:{
+                tour
+            }
+        });
+   }catch(err) {
+        res.status(400).json({
+            status: "fail",
+            message: err
+        });
+   };
+};
+
+const deleteTour = async (req,res)=>{ //delete a tour
+    try{
+        await Tour.findByIdAndDelete(req.params.id);
+        res.status(204).json({
+            status: "success",
+            data: null
+        });
+    } catch(err){
+        res.status(400).json({
+            status: "fail",
+            message: err
+        });
+    };
 };
 
 module.exports = {
